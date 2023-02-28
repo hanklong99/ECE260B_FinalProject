a101d56372faf894edaaa4b3dc403a7aa80e8c2b
Hank Long <helong@ieng6-ece-19.ucsd.edu>
2023-02-27 17:59:13 -0800

    Run dc_shell

---
+++ ece260_project/fullchip.out.v
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////
+// Created by: Synopsys DC Ultra(TM) in wire load mode
+// Version   : Q-2019.12-SP5-3
+// Date      : Mon Feb 27 17:58:09 2023
+/////////////////////////////////////////////////////////////
+
+
+module fullchip ( clk, mem_in, inst, reset );
+  input [127:0] mem_in;
+  input [16:0] inst;
+  input clk, reset;
+
+
+endmodule
+
